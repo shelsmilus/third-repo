9b8ac48a734883c59fe750f8a2ccca5157f05cff
shelsmilus <shelsmilus@gmail.com>
2024-08-28 14:51:44 -0700

    page document update

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
 
+const button = document.getElementById('button');
+
+function btnClick(event){
+  event.target.textContent = "Searching ";
+  event.target.style.color = "pink";
+  event.target.style.font = "2cm";
+}
+button.addEventListener('click', btnClick);
+
+
+
+let user;
+let pass;
+
+document.getElementById("btnsubmit").onclick = function(){
+  user = document.getElementById("nameEntered").value;
+  pass = document.getElementById("passEntered").value;
+  
+
+  console.log('Username:', user)
+  console.log('Password:', pass);
+
+}
+
+
+
+
+
+
+
+
+
+
+
 function calarea(area){
   return area**2
 }
@@ -6,3 +40,16 @@ var width = 17;
 var height = 7;
 var area = calarea(width * height)
 console.log(area);
+
+
+const maelData = [
+
+  {
+    Dish : "yamarita" ,
+    price : 17
+  },
+  {
+    Dish: "yam porridge",
+    price : 25
+  }
+]
